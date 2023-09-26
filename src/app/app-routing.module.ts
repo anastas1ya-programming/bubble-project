113c2c459fca112ed16754cdbb698a19b35dee78
anastas1ya-programming <a.yushinova77@gmail.com>
2023-09-27 00:25:36 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {MenuComponent} from "./menu/menu.component";
+import {ContactsComponent} from "./contacts/contacts.component";
+import {FranchiseComponent} from "./franchise/franchise.component";
+import {CartComponent} from "./cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path:'franchise', component: FranchiseComponent},
+  {path: 'cart', component: CartComponent},
+  {path: '**', redirectTo: '/'}
+];
+
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FranchiseComponent } from './franchise/franchise.component';
+import { CartComponent } from './cart/cart.component';
+import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuComponent,
+    ContactsComponent,
+    FranchiseComponent,
+    CartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
